8dc0dabe10b5f14ebd16545a9c9d90c0aa30b759
DavletGitacc <davletazimov142@gmail.com>
2023-02-28 18:19:35 +0600

    1

--- elreiprimo/serializers.py
+++ elreiprimo/serializers.py
@@ -7,34 +7,38 @@ class DirectorSerializer(serializers.ModelSerializer):
         fields = 'id name movie_number'.split()
     def get_movie_number(self, director):
         return director.elprimo_set.count()
-
+class ElprimoDirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = 'id name'.split()
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = 'id text stars'.split()
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = 'id name'.split()
-#
-# class TagSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Tag
-#         fields = 'id name'.split()
-
 class ElprimoSerializer(serializers.ModelSerializer):
     # category = CategorySerializer()
     # tag = TagSerializer(many=True)
     # tag_name_list = serializers.SerializerMethodField()
-    # elprimo_reviews = ReviewSerializer(many=True)
+    director = ElprimoDirectorSerializer()
+    elprimo_reviews = ReviewSerializer(many=True)
 
     class Meta:
         model = Elprimo
-        fields = '__all__'   #(id name des duration diector elprimo_reviews tag category category_name tag_name_list)
-    #
+        fields = 'id name des duration elprimo_reviews director'.split()   #( tag category category_name tag_name_list)
+
     # def get_tag_name_list(self,elprimo):
     #     lisst = []
     #     for tag in elprimo.tags.all():
     #         lisst.append(tag.name)
     #     return lisst
+
+    # class CategorySerializer(serializers.ModelSerializer):
+    #     class Meta:
+    #         model = Category
+    #         fields = 'id name'.split()
+    #
+    # class TagSerializer(serializers.ModelSerializer):
+    #     class Meta:
+    #         model = Tag
+    #         fields = 'id name'.split()
--- elreiprimo/views.py
+++ elreiprimo/views.py
@@ -5,53 +5,119 @@ from .serializers import ElprimoSerializer, DirectorSerializer, ReviewSerializer
 from .models import Elprimo, Director, Review
 from django.db.models import Avg
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def elprimo_list_api_view(request):
-    elprimos = Elprimo.objects.all()
-    serializer = ElprimoSerializer(elprimos, many=True)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        elprimos = Elprimo.objects.all()
+        serializer = ElprimoSerializer(elprimos, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        name = request.data.get('name')
+        des = request.data.get('des')
+        duration = request.data.get('duration')
+        director_id = request.data.get('director_id')
+        elprimo = Elprimo.objects.create(name=name, des=des, duration=duration, director_id=director_id)
+        return Response(data={'message': 'vse ok',
+                              'elprimo' : ElprimoSerializer(elprimo).data},
+                                status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+        # product.tags.set(tags)
+        # product.save()
+        # return Response(data={'message' : 'data recived!!!',
+        #                       'elprimo' : ElprimoSerializer(elprimo).data},
+        #                 status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def elprimo_detail_api_view(request, id):
-    try:
-        elprimo = Elprimo.objects.get(id=id)
-    except Elprimo.DoesNotExist:
-        return Response(data={'detail' : 'el primo not found!!!'},
-                        status=status.HTTP_404_NOT_FOUND)
-    serializer = ElprimoSerializer(elprimo, many = False)
-    return Response(data=serializer.data)
-
-@api_view(['GET'])
+        try:
+            elprimo = Elprimo.objects.get(id=id)
+        except Elprimo.DoesNotExist:
+            return Response(data={'detail' : 'el primo not found!!!'},
+                            status=status.HTTP_404_NOT_FOUND)
+        if request.method == 'GET':
+            serializer = ElprimoSerializer(elprimo, many = False)
+            return Response(data=serializer.data)
+        elif request.method == 'DELETE':
+            elprimo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        elif request.method == 'PUT':
+            elprimo.name = request.data.get('name')
+            elprimo.des = request.data.get('des')
+            elprimo.duration = request.data.get('duration')
+            elprimo.director_id = request.data.get('director_id')
+            # elprimo.tags.set(request.data.get('tags'))
+            elprimo.save()
+            return Response(data={'message' : 'Data recived!!!',
+                                  'elprimo' : ElprimoSerializer(elprimo).data})
+@api_view(['GET','POST'])
 def directors_list_api_view(request):
-    directors = Director.objects.all()
-    serializer = DirectorSerializer(directors, many=True)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        directors = Director.objects.all()
+        serializer = DirectorSerializer(directors, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        name = request.data.get('name')
+        director = Director.objects.create(name=name)
+        return Response(data={'message': 'vse ok',
+                              'director': DirectorSerializer(director).data},
+                        status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET','PUT', 'DELETE'])
 def director_detail_api_view(request, id):
     try:
         director = Director.objects.get(id=id)
     except Director.DoesNotExist:
         return Response(data={'detail' : 'director not found!!!'},
                         status=status.HTTP_404_NOT_FOUND)
-    serializer = DirectorSerializer(director, many = False)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = DirectorSerializer(director, many = False)
+        return Response(data=serializer.data)
+    elif request.method == 'DELETE':
+        director.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        director.name = request.data.get('name')
+        director.save()
+        return Response(data={'message' : 'Data recived!!!',
+                                'director' : DirectorSerializer(review).data})
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def review_list_api_view(request):
-    reviews = Review.objects.all()
-    serializer = ReviewSerializer(reviews, many=True)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        text = request.data.get('text')
+        stars = request.data.get('stars')
+        elprimo_id = request.data.get('elprimo_id')
+        review = Review.objects.create(text=text, stars=stars, elprimo=elprimo_id)
+        return Response(data={'message': 'vse ok',
+                              'review': ReviewSerializer(review).data},
+                        status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def review_detail_api_view(request, id):
     try:
         review = Review.objects.get(id=id)
     except Review.DoesNotExist:
         return Response(data={'detail' : 'review not found!!!'},
-                        status=status.HTTP_404_NOT_FOUND)
-    serializer = ReviewSerializer(review, many = False)
-    return Response(data=serializer.data)
+                    status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review, many = False)
+        return Response(data=serializer.data)
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        review.text = request.data.get('text')
+        review.stars = request.data.get('stars')
+        review.elprimo_id = request.data.get('elprimo_id')
+        review.save()
+        return Response(data={'message': 'Data recived!!!',
+                              'review': ReviewSerializer(review).data})
+
+
 
 @api_view(['GET'])
 def average_stars(request):
@@ -59,7 +125,7 @@ def average_stars(request):
     return Response({'average_rating' : average})
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def test_api(request):
     dict_ = {
         'text' : 'el primo',
